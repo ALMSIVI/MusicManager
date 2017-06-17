dc5c8b99266e7b583bf7bb015b32d173925512ce
ALMSIVI <zakarumbeliever@hotmail.com>
2017-06-16 18:39:42 -0700

    Add project files.

---
+++ MusicManager/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using NAudio.Wave;
+
+namespace MusicManager {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+
+    /* Main player */
+    private WaveOutEvent playController = new WaveOutEvent();
+
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    public void ButtonClicked(object sender, RoutedEventArgs e) {
+      Button button = sender as Button;
+      switch (button.Name) {
+        case "Convert":
+          Conversion conversionWindow = new Conversion();
+          conversionWindow.Show();
+          break;
+
+        case "More":
+          MoreInfo moreInfoWindow = new MoreInfo();
+          moreInfoWindow.Show();
+          break;
+      }
+    }
+  }
+}
---
+++ MusicManager/Music/Music.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+
+namespace MusicManager.Music {
+  public abstract class Music {
+    /* Basic Information, will vary from different format */
+    private string Title; // 0
+    private int TrackNo; // 1
+    private string Artist; // 2
+    private string Album; // 3
+    private string Genre; // 4
+    private int Year; // 5
+    private string Comment; // 6
+
+    /* Encoding Information */
+    private string Encoding; // 7
+    private int Channel; // 8
+    private int Frequency; // 9
+    private int Bit; // 10
+    private int Rate; // 11
+    private long Length; // 12: In milliseconds
+    private int Gain; // 13
+
+
+    /* Methods */
+    public abstract void Play();
+    public abstract void Pause();
+    public virtual string[] LoadInfo() { return new string[10]; }
+    public virtual void SaveInfo(string[] info) { }
+
+  }
+}
